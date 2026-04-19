1d657282f66ea022dd72b010769c04035f6d9784
aronas lincevicius <g00476055@atu.ie>
2026-04-19 20:37:16 +0100

    made a start on the shop page making my own logo and adding some features to the store page. very WIP

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,1 +1,12 @@
-console.log("Hello world.");
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("menuButton");
+    const dropdown = document.querySelector(".dropdown");
+
+    console.log(button, dropdown)
+
+    if (!button || !dropdown) return;
+
+    button.addEventListener("click", () => {
+        dropdown.classList.toggle("show");
+    });
+});
